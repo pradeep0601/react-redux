0322b24baf19eb4c269e7d33472876721f67293a
Pradeep Kumar <pradeepk.pdp@gmail.com>
2020-02-02 00:21:55 +0530

    connect store with react component

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
-const cakeContainer = () => {
+import {buyCake} from '../redux'
+
+const cakeContainer = (props) => {
     return (
         <div>
-            <h2>No of cakes</h2>
-            <button>Buy Cake</button>
+            <h2>No of cakes: {props.numOfCakes} </h2>
+            <button onClick = {props.buyCake}>Buy Cake</button>
         </div>
     );
 };
 
-export default cakeContainer;
+const mapStateToProps = (state) => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCake: ()=> dispatch(buyCake())
+    }
+}
+export default connect(
+    mapStateToProps, 
+    mapDispatchToProps)
+    (cakeContainer);
